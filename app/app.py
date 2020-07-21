8c9a3cbbecc63fab491e423de5752a9f897d08f1
Mateus Soares <mateus.silva@pravaler.com.br>
2020-07-20 22:55:40 -0300

    Adding python app

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+from pprint import pprint
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloWorld():
+    data = {'message': 'Hello world ;)'}
+    return jsonify(data)
+
+# start the development server using the run() method
+if __name__ == "__main__":
+    app.run()
+    
